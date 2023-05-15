d842dfb4601a81a2d8a2abd32dd3f40b5c74dcc4
gvantsa kapanadze <gvantsakapanadze95@gmail.com>
2023-05-15 23:17:46 +0400

    major functional

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,23 @@ let all = document.getElementById("all") as HTMLButtonElement;
 let active = document.getElementById("active") as HTMLButtonElement;
 let complited = document.getElementById("complited") as HTMLButtonElement;
 let boxWrapper = document.getElementById("box-wrapper") as HTMLElement;
+let checkBox = document.getElementsByClassName("checkbox") as HTMLCollection;
 
 input.addEventListener("keydown", (event: any) => {
   if (event.key == "Enter" && event.target.value != "") {
-    let image = document.createElement("img") as HTMLImageElement;
-    image.src = ".././images/icon-cross.svg";
+    boxWrapper.innerHTML += `
+      <div class="box"> 
+        <input type="checkbox" class="checkbox" id="checkbox" />
+        <span class="custom_checkbox" id="custom_checkbox"></span>
+        <p>${event.target.value}</p>
+        <img src=".././images/icon-cross.svg">
+      </div>
+    `;
 
-    let checkBox = document.createElement("input") as HTMLInputElement;
-    let custom_checkbox = document.createElement("span") as HTMLSpanElement;
-    checkBox.type = "checkbox";
-
-    let value = event.target.value;
-    const box = document.createElement("div");
-    box.classList.add("box");
-    boxWrapper.appendChild(box);
-    box.appendChild(checkBox);
-    box.appendChild(custom_checkbox);
-    const pTag = document.createElement("p");
-    pTag.innerText = value;
-    box.appendChild(pTag);
-    event.target.value = "";
-    box.appendChild(image);
-    box.style.marginTop = "20px";
+    for (let element of boxWrapper.children) {
+      element.firstElementChild?.addEventListener("click", () => {
+        element.classList.add("check");
+      });
+    }
   }
 });
