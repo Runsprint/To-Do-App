3793c5e08c0ebea971cac0978e04966ebe0a7f5f
gvantsa kapanadze <gvantsakapanadze95@gmail.com>
2023-05-18 20:38:19 +0400

    dont write drag and drop, also type errors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,25 @@ let all = document.getElementById("all") as HTMLButtonElement;
 let active = document.getElementById("active") as HTMLButtonElement;
 let completed = document.querySelector("#completed") as HTMLElement;
 let boxWrapper = document.getElementById("box-wrapper") as HTMLElement;
-
 let items = document.getElementById("items") as HTMLElement;
 let items_left = document.getElementById("items_left") as HTMLElement;
 let clear = document.getElementById("clear") as HTMLElement;
 let textArray: string[] = []; //local storage, set and get items. json.stringify, tringad gadavakcev json.parse.
+
+let sun = document.getElementById("sun");
+let moon = document.getElementById("moon");
+
+if (moon !== null && sun !== null) {
+  moon.addEventListener("click", () => {
+    sun.style.display = "flex";
+    moon.style.display = "none";
+  });
+
+  sun.addEventListener("click", () => {
+    sun.style.display = "none";
+    moon.style.display = "flex";
+  });
+}
 
 input.addEventListener("keydown", (event: KeyboardEvent) => {
   if (
@@ -16,15 +30,45 @@ input.addEventListener("keydown", (event: KeyboardEvent) => {
     (event.target as HTMLInputElement).value !== ""
   ) {
     textArray.push((event.target as HTMLInputElement).value);
-    console.log(textArray);
-    let newDiv = document.createElement("div");
+
+    const newDiv = document.createElement("div");
     newDiv.classList.add("box");
+    newDiv.id = "divId";
     boxWrapper.prepend(newDiv);
+
+    // newDiv.draggable = true;
+    // newDiv.addEventListener("dragstart", drag);
+    // newDiv.addEventListener("dragend", drag);
+    // boxWrapper.addEventListener("dragover", allowDrop);
+    // boxWrapper.addEventListener("drop", drop);
+
+    // let targetEllement: null | HTMLElement = null;
+    // function drag(event: DragEvent) {
+    //   targetEllement = event.target as HTMLElement;
+    //   if (event.dataTransfer) {
+    //     event.dataTransfer.setData("div", targetEllement.id);
+    //   }
+    // }
+
+    // function allowDrop(event: DragEvent) {
+    //   event.preventDefault();
+    // }
+
+    // function drop(event: DragEvent) {
+    //   event.preventDefault();
+    //   const data = event.dataTransfer?.getData("div");
+    //   const draggedElement = document.getElementById(data!);
+
+    //   if (draggedElement && boxWrapper) {
+    //     boxWrapper.appendChild(draggedElement);
+    //   }
+    // }
 
     let checkBox = document.createElement("input");
     checkBox.classList.add("checkbox");
     checkBox.type = "checkbox";
     newDiv.append(checkBox);
+    newDiv.classList.add("newDiv");
 
     let custom_checkbox = document.createElement("span");
     checkBox.classList.add("custom_checkbox");
@@ -33,6 +77,23 @@ input.addEventListener("keydown", (event: KeyboardEvent) => {
     let p = document.createElement("p");
     p.innerHTML = (event.target as HTMLInputElement).value;
     newDiv.append(p);
+    p.classList.add("p");
+
+    var img = new Image();
+    img.src = "./images/icon-cross.svg";
+    newDiv.append(img);
+    img.classList.add("img");
+
+    img.addEventListener("click", () => {
+      let counter = boxWrapper.children.length;
+      newDiv.remove();
+      counter--;
+      items_left.innerHTML = counter + "ragac warwera";
+      if (p.textContent !== null) {
+        let index = textArray.indexOf(p.textContent);
+        textArray.splice(index, 1);
+      }
+    });
 
     items_left.innerHTML = textArray.length + "ragac warwera";
     if (textArray.length > 0) {
@@ -100,6 +161,6 @@ clear.addEventListener("click", () => {
       counter--;
     }
   }
-  console.log(textArray);
+
   items_left.innerHTML = counter + "ragac warwera";
 });
